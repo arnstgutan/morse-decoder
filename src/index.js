75acc295672a666195101f58d92249930f0fe0f8
arnstgutan <arnstsasha92@gmail.com>
2022-02-21 13:38:00 +0300

    MORSE COMPLETE

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let LetterCodes = []
+    let size = 10;
+    let exprArray = expr.split('').reverse()
+    let result = []
+    for (i = 0; i < Math.ceil(exprArray.length/size); i++ ) {
+        LetterCodes[i] = exprArray.slice((i*size), (i*size) + size).reverse()
+    }
+    LetterCodes = LetterCodes.reverse()
+    for (let letterBinary = 0; letterBinary < LetterCodes.length; letterBinary++) {
+        let morse = []
+        if (LetterCodes[letterBinary].includes('*') ) {
+            result.push('-')
+        }
+
+        if (LetterCodes[letterBinary].length < size) {
+             while (LetterCodes[letterBinary].length < size) {
+                LetterCodes[letterBinary].unshift('0')
+            } 
+        }
+        for (let j = 0; j <= 8; j += 2) {
+            if (LetterCodes[letterBinary][j] + LetterCodes[letterBinary][j + 1] === '00') {
+                morse.push('')
+            } else if (LetterCodes[letterBinary][j] + LetterCodes[letterBinary][j + 1] === '10') {
+                morse.push('.')
+            } else if (LetterCodes[letterBinary][j] + LetterCodes[letterBinary][j + 1] === '11') {
+                morse.push('-')
+            }
+        }
+        morse = morse.join('')
+        result.push(MORSE_TABLE[morse])
+    }
+    return result.join('').replace(/-/g, ' ');
 }
 
 module.exports = {
